da73e5880faaf41ebe8a895e83c390f52d924cfa
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-01-30 20:27:33 +0600

    card descriptio change

--- src/FakeData/SliderInfo.js
+++ src/FakeData/SliderInfo.js
@@ -2,20 +2,20 @@ const SliderInfo = [
     {
         id: 1,
         tittle:'SUNDAR BAN',
-        description:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab pariatur unde, ea aspernatur quasi corporis, atque recusandae fugit veritatis aliquid tempora et vitae architecto reprehenderit numquam, voluptatum libero. Obcaecati.',
+        description:' The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in Indias state of West Bengal to the Baleswar River in Bangladesh.',
         image:'https://i.imgur.com/t7JvdK4.png'
     
     },
     {
         id: 2,
         tittle:'SAJEK',
-        description:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab pariatur unde, ea aspernatur quasi corporis, atque recusandae fugit veritatis aliquid tempora et vitae architecto reprehenderit numquam, voluptatum libero. Obcaecati.',
+        description:'Sajek valley is known for its natural environment and is surrounded by mountains, dense forest, and grassland hill tracks. Many small rivers flow through the mountains among which the Kachalong and the Machalong are notable. On the way to Sajek valley, one has to cross the Mayni range and the Mayni river.',
         image:'https://i.imgur.com/sp4KtiC.png'
     },
     {
         id: 3,
         tittle:'COXES BAZAR',
-        description:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab pariatur unde, ea aspernatur quasi corporis, atque recusandae fugit veritatis aliquid tempora et vitae architecto reprehenderit numquam, voluptatum libero. Obcaecati.',
+        description:' Coxs Bazar (Bengali: কক্সবাজার জেলা, Coxs Bazar Jela also Coxs Bazar Zila) is a district in the Chittagong Division of Bangladesh.[2] It is named after Coxs Bazar town.',
         image:'https://i.imgur.com/ZLf6jFg.png'
     },
 ];
